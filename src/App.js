2138ca2b1915041b60a399ce60da5fd79e86e6ce
Mike James <m@mikejam.es>
2017-11-20 16:40:31 +1100

    example styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import BasicExample from './BasicExample';
 import PopulateExample from './PopulateExample';
@@ -11,12 +11,18 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Formzy</h1>
+          <code>
+            yarn add formzy
+          </code>
         </header>
         <p className="App-intro">
           To get started, try the example.
         </p>
         <BasicExample />
         <PopulateExample />
+        <p className="App-intro">
+
+        </p>
       </div>
     );
   }
